b5d22b75e4d44902c5f544a5294df63ab8dd8d76
Paul Martin <greatestloginnameever@gmail.com>
2019-10-27 17:14:47 +0000

    Remove unneeded utils

--- aionetworking/utils.py
+++ aionetworking/utils.py
@@ -4,11 +4,8 @@ from aiofiles.os import wrap
 from collections import ChainMap
 from ipaddress import AddressValueError
 import builtins
-import datetime
-import io
 import operator
 import os
-import struct
 import re
 import traceback
 import time
@@ -21,8 +18,8 @@ from functools import wraps
 
 from .compatibility import Protocol
 from pathlib import Path
-from typing import Sequence, Callable, List, AnyStr, Tuple, Union, Iterator, AsyncGenerator, Any, TYPE_CHECKING, Generator, Iterable
-from ipaddress import IPv4Address, IPv6Address, IPv4Network, IPv6Network
+from typing import Sequence, Callable, List, AnyStr, Tuple, Union, AsyncGenerator, Any, TYPE_CHECKING, Generator, Iterable
+from ipaddress import IPv4Network, IPv6Network
 
 try:
     import psutil
@@ -48,7 +45,6 @@ async def time_coro(coro):
     return time.time() - start_time
 
 
-###Coroutines###
 async def benchmark(async_func: Callable, *args, num_times: int = 5, quiet: bool = False, cleanup: Callable = None,
                     cleanup_args=(), num_items: int = None, num_bytes: int = None, ignore_first: bool = True, **kwargs):
     times = []
@@ -83,16 +79,34 @@ async def benchmark(async_func: Callable, *args, num_times: int = 5, quiet: bool
 
 
 ###Dataclasses###
-def compare_dataclasses(dc1, dc2) -> Generator[str, None, None]:
-    for f in fields(dc1):
-        value1 = getattr(dc1, f.name)
-        value2 = getattr(dc2, f.name)
-        if  value1 != value2:
-            if f.compare:
-                yield f.name, value1, value2
+
+def dataclass_getstate(self):
+    state = {}
+    f = fields(self)
+    for field in f:
+        name = field.name
+        if field.init:
+            try:
+                value = getattr(self, field.name)
+                if field.default == MISSING and field.default_factory == MISSING:
+                    state[name] = value
+                elif field.default != MISSING and value != field.default:
+                    if field.metadata.get('pickle', True):
+                        state[name] = value
+                elif field.default_factory != MISSING:
+                    if field.metadata.get('pickle', False):
+                        state[name] = value
+            except AttributeError:
+                pass
+    return state
+
+
+def dataclass_setstate(self, state):
+    self.__dict__.update(self.__class__(**state).__dict__)
 
 
 ###Typing###
+
 class EmptyProtocol(Protocol):
     pass
 
@@ -107,16 +121,8 @@ def inherit_on_type_checking_only(arg):
     return EmptyProtocol
 
 
-###Iterators###
-"""def has_items(generator: Iterator) -> bool:
-    try:
-        next(generator)
-        return True
-    except StopIteration:
-        return False"""
-
-
 ###Async Generators###
+
 async def aone(generator: AsyncGenerator) -> Any:
     item = await generator.__anext__()
     await generator.aclose()
@@ -178,27 +184,6 @@ def log_exception(ex: BaseException) -> Sequence[str]:
                 traceback.format_exception(ex.__class__, ex, ex.__traceback__)]
 
 
-###Datetime utils###
-
-"""def timestamp_to_utc_string(dt) -> str:
-    return datetime.datetime.strftime(dt, '%Y%m%d%H%M%S')
-
-
-def now_to_utc_string() -> str:
-    return timestamp_to_utc_string(datetime.datetime.now())
-
-
-def datetime_to_human_readable(dt: datetime.datetime, strf: str='%Y-%m-%d %H:%M:%S.%f') -> str:
-    if not dt.microsecond:
-        strf = strf.replace('.%f', '')
-        return dt.strftime(strf)
-    return dt.strftime(strf)[:-5]
-
-
-def current_date() -> str:
-    return datetime.datetime.now().strftime("%Y%m%d")"""
-
-
 ###Multiprocessing###
 
 _mmap_counter = itertools.count()
@@ -232,146 +217,7 @@ def pipe_address_by_os() -> Path:
     OSError("Neither AF_UNIX nor Named Pipe is supported on this platform")
 
 
-###Binary###
-
-"""def pack_variable_len_string(content: bytes) -> bytes:
-    return struct.pack("I", len(content)) + content
-
-
-def unpack_variable_len_bytes(pos: int, content: bytes) -> (int, bytes):
-    int_size = struct.calcsize("I")
-    length = struct.unpack("I", content[pos:pos + int_size])[0]
-    pos += int_size
-    end_byte = pos + length
-    data = content[pos:end_byte]
-    return end_byte, data
-
-
-def unpack_variable_len_string(*args) -> (int, str):
-    end_byte, data = unpack_variable_len_bytes(*args)
-    return end_byte, data.decode()
-
-
-def unpack_variable_len_strings(content: bytes) -> List[AnyStr]:
-    pos = 0
-    bytes_list = []
-    while pos < len(content):
-        pos, string = unpack_variable_len_string(pos, content)
-        bytes_list.append(string)
-    return bytes_list
-
-
-###Recording###
-
-class Record:
-    first_msg_time = 0
-
-    @classmethod
-    def from_file(cls, path):
-        if isinstance(path, io.BufferedReader):
-            content = path.read()
-        else:
-            content = path.read_bytes()
-        float_size = struct.calcsize("f")
-        bool_size = struct.calcsize("?")
-        pos = 0
-        while pos < len(content):
-            sent_by_server = struct.unpack("?", content[pos:pos + bool_size])[0]
-            pos += bool_size
-            seconds = struct.unpack("f", content[pos:pos + float_size])[0]
-            pos += float_size
-            is_bytes = struct.unpack("?", content[pos:pos + bool_size])[0]
-            pos += bool_size
-            pos, peer = unpack_variable_len_string(pos, content)
-            pos, packet_data = unpack_variable_len_bytes(pos, content)
-            if not is_bytes:
-                packet_data = packet_data.decode()
-            yield ({'sent_by_server': sent_by_server,
-                    'seconds': seconds,
-                    'peer': peer,
-                    'data': packet_data})
-
-    def pack_server_msg(self, msg):
-        return self.pack_recorded_packet(True, msg)
-
-    def pack_client_msg(self, msg):
-        return self.pack_recorded_packet(False, msg)
-
-    def pack_recorded_packet(self, sent_by_server: bool, msg) -> bytes:
-        if self.first_msg_time:
-            time_delta = msg.received_timestamp - self.first_msg_time
-            seconds = time_delta.seconds + round(time_delta.microseconds / 1000000)
-        else:
-            self.first_msg_time = msg.received_timestamp
-            seconds = 0
-        if isinstance(msg.encoded, bytes):
-            is_bytes = True
-            encoded = msg.encoded
-        else:
-            is_bytes = False
-            encoded = msg.encoded.encode()
-        return struct.pack('?', sent_by_server) + struct.pack('f', seconds) + struct.pack('?', is_bytes) + pack_variable_len_string(
-            msg.sender.encode()) + pack_variable_len_string(encoded)
-
-
-def unpack_recorded_packets(content: bytes) -> Sequence[Tuple[int, AnyStr, bytes]]:
-    int_size = struct.calcsize("I")
-    pos = 0
-    packets = []
-    while pos < len(content):
-        seconds = struct.unpack("I", content[pos:pos+int_size])[0]
-        pos += int_size
-        pos, sender = unpack_variable_len_string(pos, content)
-        pos, packet_data = unpack_variable_len_string(pos, content)
-        packets.append((seconds, sender, packet_data))
-    return packets
-
-#Text
-def camel_case_to_title(string: str) -> str:
-    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1 \2', string)
-    return re.sub('([a-z0-9])([A-Z])', r'\1 \2', s1).title()"""
-
-
-#Text formatting
-"""class Color:
-    PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    DARKCYAN = '\033[36m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    END = '\033[0m'
-
-
-def bold(text: str) -> str:
-    return Color.BOLD + text + Color.END
-
-
-def underline(text: str) -> str:
-    return Color.UNDERLINE + text + Color.END"""
-
-
-###Testing###
-"""async def run_and_wait(method, *args, interval: int=7, **kwargs):
-    await method(*args, **kwargs)
-    await asyncio.sleep(interval)
-
-
-async def run_wait_close(method, message_manager, *args, interval: int=1, **kwargs):
-    await run_and_wait(method, *args, interval=interval, **kwargs)
-    await message_manager.stop()
-
-
-async def run_wait_close_multiple(method, message_manager, sender, msgs: [Sequence],
-                                  interval: int=1, final_interval: int=5, **kwargs):
-    for message in msgs:
-        await run_and_wait(method, sender, message, interval=interval, **kwargs)
-    await asyncio.sleep(final_interval)
-    await message_manager.stop()"""
-
+###Networking###
 
 def addr_tuple_to_str(addr: Sequence):
     return ':'.join(str(a) for a in addr)
@@ -380,47 +226,6 @@ def addr_tuple_to_str(addr: Sequence):
 def addr_str_to_tuple(addr: AnyStr):
     addr, port = addr.split(':')
     return addr, int(port)
-
-
-def dataclass_getstate(self):
-    state = {}
-    f = fields(self)
-    for field in f:
-        name = field.name
-        if field.init:
-            try:
-                value = getattr(self, field.name)
-                if field.default == MISSING and field.default_factory == MISSING:
-                    state[name] = value
-                elif field.default != MISSING and value != field.default:
-                    if field.metadata.get('pickle', True):
-                        state[name] = value
-                elif field.default_factory != MISSING:
-                    if field.metadata.get('pickle', False):
-                        state[name] = value
-            except AttributeError:
-                pass
-    return state
-
-
-def dataclass_setstate(self, state):
-    self.__dict__.update(self.__class__(**state).__dict__)
-
-
-class SystemInfo:
-    @property
-    def memory(self):
-        try:
-            return psutil.Process(os.getpid()).memory_info()[0]/2.**30
-        except NameError:
-            return "Unknown"
-
-    @property
-    def cpu(self):
-        try:
-            return psutil.Process(os.getpid()).cpu_percent()
-        except NameError:
-            return "Unknown"
 
 
 class IPNetwork:
@@ -458,6 +263,26 @@ def supernet_of(network: Union[str, IPNetwork, IPv4Network, IPv6Network], networ
         networks = filter(lambda n: not n.is_ipv6, networks)
     return any(n.supernet_of(network.ip_network) for n in networks)
 
+
+###System###
+
+class SystemInfo:
+    @property
+    def memory(self):
+        try:
+            return psutil.Process(os.getpid()).memory_info()[0]/2.**30
+        except NameError:
+            return "Unknown"
+
+    @property
+    def cpu(self):
+        try:
+            return psutil.Process(os.getpid()).cpu_percent()
+        except NameError:
+            return "Unknown"
+
+
+###Misc###
 
 @dataclass
 class CallableFromString(Callable):
